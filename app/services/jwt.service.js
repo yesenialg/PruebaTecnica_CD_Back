5976556480313eab135625cd6be8df30c4eec4c2
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2021-06-12 09:07:55 -0500

    Token 6horas

--- app/services/jwt.service.js
+++ app/services/jwt.service.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = "5IO&!:S1#!w.^hVU;Kt[5m8jy+@sT7;%fW'{*W/}L5CEbk~XgbF4c!es%p-Qy&Y";
 
 const sign = (data) => {
-    return jwt.sign(data, SECRET_KEY, {expiresIn: "6days"});
+    return jwt.sign(data, SECRET_KEY, {expiresIn: "6h"});
 }
 
 const verify = (token) => {
